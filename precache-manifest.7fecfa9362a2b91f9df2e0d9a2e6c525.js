d383b9284ab2e24aa536f1f402fdb50ea01f4317
mandolyte <cecil.new@gmail.com>
2021-04-02 15:50:16 -0400

    Updates

---
+++ precache-manifest.7fecfa9362a2b91f9df2e0d9a2e6c525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24c6874b6237b5f3954f8fd2f9da614",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb211c48a479d8b8fd7e",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "b116231a806bd871a6e9",
+    "url": "/static/js/2.d62331ef.chunk.js"
+  },
+  {
+    "revision": "7c76c3570331b176a80a94dfc9d08b5e",
+    "url": "/static/js/2.d62331ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb211c48a479d8b8fd7e",
+    "url": "/static/js/main.52652430.chunk.js"
+  },
+  {
+    "revision": "e82b8dd9ea03d8144f96",
+    "url": "/static/js/runtime-main.e82a769f.js"
+  }
+]);
